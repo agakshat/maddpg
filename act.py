432093eedbd1550e43eef23f141475b73df289ad
agakshat <agarwalaks30@gmail.com>
2017-11-17 11:43:17 -0500

    act

--- act.py
+++ act.py
@@ -5,15 +5,15 @@ import gym
 from actorcriticv2 import ActorNetwork,CriticNetwork
 
 actors = []
-actors.append(load_model('actor0main0.h5'))
-actors.append(load_model('actor1main0.h5'))
-actors.append(load_model('actor2main0.h5'))
+actors.append(load_model('results/actor0/main16000.h5'))
+actors.append(load_model('results/actor1/main16000.h5'))
+actors.append(load_model('results/actor2/main16000.h5'))
 
 env = make_env.make_env('simple_spread')
 while(1):
 	for i in range(env.n):
 		actor = actors[i]
-		a.append(actor.act(np.reshape(s[i],(-1,actor.state_dim)),noise[i]()).reshape(actor.action_dim,))
+		a.append((actor.predict(np.reshape(s[i],(-1,actor.state_dim)))+noise[i]()).reshape(actor.action_dim,))
 
 	s2,r,done,_ = env.step(a) # a is a list with each element being an array
 	env.render()
