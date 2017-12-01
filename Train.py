gumbel-softmax added, gradients corrected

Train.py
@@ -3,22 +3,20 @@ import gym
 import tensorflow as tf
 import random
 from ReplayMemory import ReplayMemory
-#from actorcritic import ActorNetwork,CriticNetwork
 
-def build_summaries(n):
-	#episode_reward = tf.get_variable("episode_reward",[1,n])
-	episode_reward =   tf.Variable(0.)
-	tf.summary.scalar("Reward",episode_reward)
-	#episode_ave_max_q = tf.Variable("episode_av_max_")
-	#tf.summary.scalar("QMaxValue",episode_ave_max_q)
-	#summary_vars = [episode_reward,episode_ave_max_q]
-	summary_vars = [episode_reward]
+def build_summaries():
+	episode_reward1 = tf.Variable(0.)
+	tf.summary.scalar("Reward1",episode_reward1)
+	episode_reward2 = tf.Variable(0.)
+	tf.summary.scalar("Reward2",episode_reward2)
+	summary_vars = [episode_reward1,episode_reward2]
 	summary_ops = tf.summary.merge_all()
+
 	return summary_ops, summary_vars
 
 def train(sess,env,args,actors,critics,noise):
 
-	summary_ops,summary_vars = build_summaries(env.n)
+	summary_ops,summary_vars = build_summaries()
 	init = tf.global_variables_initializer()
 	sess.run(init)
 	writer = tf.summary.FileWriter(args['summary_dir'],sess.graph)
@@ -33,36 +31,35 @@ def train(sess,env,args,actors,critics,noise):
 	for ep in range(int(args['max_episodes'])):
 		s = env.reset()
 		episode_reward = np.zeros((env.n,))
-		#episode_av_max_q = 0
+
 		if ep%100==0:
 			for k in range(env.n):
 				file1 = 'results/actor'+str(k)+'/main'+str(ep)+'.h5'
-				file2 = 'results/actor'+str(k)+'/target'+str(ep)+'.h5'
+				#file2 = 'results/actor'+str(k)+'/target'+str(ep)+'.h5'
 				file3 = 'results/critic'+str(k)+'/main'+str(ep)+'.h5'
-				file4 = 'results/critic'+str(k)+'/target'+str(ep)+'.h5'
+				#file4 = 'results/critic'+str(k)+'/target'+str(ep)+'.h5'
 				actor = actors[k]
 				critic = critics[k]
 				actor.mainModel.save(file1)
-				actor.targetModel.save(file2)
+				#actor.targetModel.save(file2)
 				critic.mainModel.save(file3)
-				critic.targetModel.save(file4)
+				#critic.targetModel.save(file4)
+
 		for stp in range(int(args['max_episode_len'])):
 			if args['render_env']:
 				env.render()
-			#print("episode {}, step {}".format(ep,stp))
-			a = []
-			action_dims_done = 0
 
+			a = []
 			for i in range(env.n):
 				actor = actors[i]
 				a.append(actor.act(np.reshape(s[i],(-1,actor.state_dim)),noise[i]()).reshape(actor.action_dim,))
-						
+			print("Action: {}".format(a))
 			s2,r,done,_ = env.step(a) # a is a list with each element being an array
-			#replayMemory.add(np.reshape(s,(actor.input_dim,)),np.reshape(a,(actor.output_dim,)),r,done,np.reshape(s2,(actor.input_dim,)))
 			replayMemory.add(s,a,r,done,s2)
 			s = s2
 
 			for i in range(env.n):
+				action_dims_done = 0
 				actor = actors[i]
 				critic = critics[i]
 				if replayMemory.size()>int(args['minibatch_size']):
@@ -85,9 +82,7 @@ def train(sess,env,args,actors,critics,noise):
 						else:
 							yi.append(r_batch[:,i][k] + critic.gamma*targetQ[k])
 					s_batch_i = np.asarray([x for x in s_batch[:,i]])
-					critic.train(s_batch_i,np.asarray([x.flatten() for x in a_batch]),np.asarray(yi))
-					#predictedQValue = critic.train(s_batch,np.asarray([x.flatten() for x in a_batch]),yi)
-					#episode_av_max_q += np.amax(predictedQValue)
+					critic.train(s_batch_i,np.asarray([x.flatten() for x in a_batch]),np.reshape(yi,(int(args['minibatch_size']),1)))
 					
 					actions_pred = []
 					for j in range(env.n):
@@ -99,23 +94,22 @@ def train(sess,env,args,actors,critics,noise):
 					s_batch_i = np.asarray([x for x in s_batch[:,i]])
 					grads = critic.action_gradients(s_batch_i,a_for_critic_pred)[:,action_dims_done:action_dims_done + actor.action_dim]
 					actor.train(s_batch_i,grads)
-					#print("Training agent {}".format(i))
 					actor.update_target()
 					critic.update_target()
-
-			action_dims_done = action_dims_done + actor.action_dim
+				
+				action_dims_done = action_dims_done + actor.action_dim
 			episode_reward += r
 			#print(done)
 			if np.all(done):
 				#summary_str = sess.run(summary_ops, feed_dict = {summary_vars[0]: episode_reward, summary_vars[1]: episode_av_max_q/float(stp)})
-				summary_str = sess.run(summary_ops, feed_dict = {summary_vars[0]: np.sum(episode_reward)})
+				summary_str = sess.run(summary_ops, feed_dict = {summary_vars[0]: episode_reward[0], summary_vars[1]: episode_reward[3]})
 				writer.add_summary(summary_str,ep)
 				writer.flush()
 				#print ('|Reward: {:d}| Episode: {:d}| Qmax: {:.4f}'.format(int(episode_reward),ep,(episode_av_max_q/float(stp))))
-				print ('|Reward: {:d},{:d},{:d}	| Episode: {:d}'.format(int(episode_reward[0]),int(episode_reward[1]),int(episode_reward[2]),ep))
+				print ('|Reward: {}	| Episode: {:d}'.format(episode_reward,ep))
 				break
 
 			if stp == int(args['max_episode_len'])-1:
-				print ('|Reward: {:d},{:d},{:d},{:d}	| Episode: {:d}'.format(int(episode_reward[0]),int(episode_reward[1]),int(episode_reward[2]),int(episode_reward[3]),ep))
+				print ('|Reward: {}	| Episode: {:d}'.format(episode_reward,ep))
 
 

actorcriticv2.py
@@ -1,9 +1,10 @@
 import tensorflow as tf
-import numpy as  np
+import numpy as np
 from keras.models import Model
-from keras.layers import Dense,Input,BatchNormalization,Concatenate,Activation
+from keras.layers import Dense,Input,BatchNormalization,Add,Activation,Lambda
 from keras.optimizers import Adam
 import keras.backend as K
+#from utils import onehot_argmax_layer
 
 class ActorNetwork(object):
 	"""
@@ -24,27 +25,18 @@ class ActorNetwork(object):
 		grads = zip(self.params_grad,self.mainModel_weights)
 		self.optimize = tf.train.AdamOptimizer(self.lr).apply_gradients(grads)
 		self.sess.run(tf.global_variables_initializer())
-	'''
-	def _build_model(self):
-		model = Sequential()
-		model.add(Dense(64,input_dim = self.state_dim, activation = 'relu'))
-		model.add()
-		model.add(Dense(64, activation = 'relu'))
-		model.add(Dense(self.action_dim, activation = 'softmax'))
-		model.compile(optimizer=Adam,loss='categorical_crossentropy',metrics=['accuracy'])
-		return model
-	'''
+
 	def _build_model(self):
 		input_obs = Input(shape=(self.state_dim,))
-		h = Dense(64)(input_obs)
+		h = Dense(400)(input_obs)
 		h = Activation('relu')(h)
-		h = BatchNormalization()(h)
-		h = Dense(64)(h)
+		#h = BatchNormalization()(h)
+		h = Dense(300)(h)
 		h = Activation('relu')(h)
-		h = BatchNormalization()(h)
+		#h = BatchNormalization()(h)
 		h = Dense(self.action_dim)(h)
-		pred = Activation('softmax')(h)
-		#pred = tf.contrib.distributions.RelaxedOneHotCategorical(0.1,probs=h).sample()
+		h = Activation('softmax')(h)
+		pred = Lambda(lambda h: tf.contrib.distributions.RelaxedOneHotCategorical(0.5,probs=h).sample())(h)
 		model = Model(inputs=input_obs,outputs=pred)
 		model.compile(optimizer='Adam',loss='categorical_crossentropy')
 		return model,model.trainable_weights,input_obs
@@ -62,7 +54,9 @@ class ActorNetwork(object):
 	def update_target(self):
 		wMain =  np.asarray(self.mainModel.get_weights())
 		wTarget = np.asarray(self.targetModel.get_weights())
-		self.targetModel.set_weights(self.tau*wMain + (1.0-self.tau)*wTarget)
+		for i in range(len(wMain)):
+			wTarget[i] = self.tau*wMain[i] + (1-self.tau)*wTarget[i]
+		self.targetModel.set_weights(wTarget)
 
 	def train(self,state,action_grad):
 		self.sess.run(self.optimize,feed_dict = {self.mainModel_state: state, self.action_gradient: action_grad})
@@ -87,16 +81,17 @@ class CriticNetwork(object):
 	def _build_model(self):
 		input_obs = Input(shape=(self.state_dim,))
 		input_actions = Input(shape=(self.action_dim,))
-		h = Dense(64)(input_obs)
+		h = Dense(400)(input_obs)
 		h = Activation('relu')(h)
-		h = BatchNormalization()(h)
-		action_abs = Dense(64)(input_actions)
-		action_abs = Activation('relu')(action_abs)
-		action_abs = BatchNormalization()(action_abs)
-		h = Concatenate()([h,action_abs])
-		h = Dense(64)(h)
+		#h = BatchNormalization()(h)
+		action_abs = Dense(300)(input_actions)
+		temp1 = Dense(300)(h)
+		#action_abs = Activation('relu')(action_abs)
+		#action_abs = BatchNormalization()(action_abs)
+		h = Add()([temp1,action_abs])
+		#h = Dense(64)(h)
 		h = Activation('relu')(h)
-		h = BatchNormalization()(h)
+		#h = BatchNormalization()(h)
 		pred = Dense(1,kernel_initializer='random_uniform')(h)
 		model = Model(inputs=[input_obs,input_actions],outputs=pred)
 		model.compile(optimizer='Adam',loss='mean_squared_error')
@@ -108,7 +103,9 @@ class CriticNetwork(object):
 	def update_target(self):
 		wMain =  np.asarray(self.mainModel.get_weights())
 		wTarget = np.asarray(self.targetModel.get_weights())
-		self.targetModel.set_weights(self.tau*wMain + (1.0-self.tau)*wTarget)
+		for i in range(len(wMain)):
+			wTarget[i] = self.tau*wMain[i] + (1-self.tau)*wTarget[i]
+		self.targetModel.set_weights(wTarget)
 
 	def predict_target(self, state, actions):
 		return self.targetModel.predict([state,actions])
@@ -121,4 +118,3 @@ class CriticNetwork(object):
 
 	def train(self, state, actions, labels):
 		self.mainModel.train_on_batch([state,actions],labels)
-		#return self.predict(state,actions)

training-code.py
@@ -13,6 +13,11 @@ def main(args):
 
     with tf.Session() as sess:
         env  = make_env.make_env('simple_tag')
+
+        np.random.seed(int(args['random_seed']))
+        tf.set_random_seed(int(args['random_seed']))
+        env.seed(int(args['random_seed']))
+
         n = env.n
         actors = []
         critics = []
@@ -20,6 +25,7 @@ def main(args):
         observation_dim = []
         action_dim = []
         total_action_dim = 0
+        
         for i in range(n):
             total_action_dim = total_action_dim + env.action_space[i].n
         for i in range(n):
@@ -43,10 +49,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='provide arguments for DDPG agent')
 
     # agent parameters
-    parser.add_argument('--actor-lr', help='actor network learning rate', default=0.01)
-    parser.add_argument('--critic-lr', help='critic network learning rate', default=0.01)
+    parser.add_argument('--actor-lr', help='actor network learning rate', default=0.0001)
+    parser.add_argument('--critic-lr', help='critic network learning rate', default=0.001)
     parser.add_argument('--gamma', help='discount factor for critic updates', default=0.99)
-    parser.add_argument('--tau', help='soft target update parameter', default=0.01)
+    parser.add_argument('--tau', help='soft target update parameter', default=0.001)
     parser.add_argument('--buffer-size', help='max size of the replay buffer', default=1000000)
     parser.add_argument('--minibatch-size', help='size of minibatch for minibatch-SGD', default=64)
 